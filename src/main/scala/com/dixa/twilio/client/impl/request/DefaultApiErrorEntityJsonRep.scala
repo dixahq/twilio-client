82ed7814381de3579f64019f2fd6a495b2503de6
Anders Kreinøe <ankr@dixa.com>
2022-02-04 10:21:05 +0100

    Added scaladoc to DefaultApiErrorEntityJsonRep including a link to twilio documentation.

--- src/main/scala/com/dixa/twilio/client/impl/request/DefaultApiErrorEntityJsonRep.scala
+++ src/main/scala/com/dixa/twilio/client/impl/request/DefaultApiErrorEntityJsonRep.scala
@@ -1,5 +1,10 @@
 package com.dixa.twilio.client.impl.request
 
+/** Json representation of the standard error entity that Twilio will send back on none 200
+  * responses.
+  *
+  * More details can be found here: https://www.twilio.com/docs/api/errors
+  */
 private[request] final case class DefaultApiErrorEntityJsonRep(
     code: Long,
     message: String,
