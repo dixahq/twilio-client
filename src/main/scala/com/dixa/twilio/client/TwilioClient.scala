ea2c8d69c651c852a7edd90a9590ffac97e5eed4
Anders Kreinøe <ankr@dixa.com>
2022-02-22 13:22:09 +0100

    Added missing scaladoc to the client package, SingleRequestExecutor and TwilioClient

--- src/main/scala/com/dixa/twilio/client/TwilioClient.scala
+++ src/main/scala/com/dixa/twilio/client/TwilioClient.scala
@@ -7,14 +7,38 @@ import com.dixa.twilio.client.messaging.TwilioClientMessaging
 import com.dixa.twilio.client.phonenumber.TwilioClientPhoneNumber
 import com.dixa.twilio.client.voice.TwilioClientVoice
 
+/** Main entry point for communicating with twilio.
+  *
+  * You can get an instance of this via [[com.dixa.twilio.client.TwilioClient.defaultImpl()]]
+  *
+  * It does not in itself provide any twilio calls directly, but instead provide access to a sub
+  * client for each of Twilios sub APIs.
+  *
+  * The sub clients then provide methods for performing calls against twilio. Such request can be of
+  * two different types:
+  *
+  * 1) A standard request. A method for this will return an implementation of
+  * [[com.dixa.twilio.client.SingleRequestExecutor]]. Look at the documentation of that trait for
+  * more details.
+  *
+  * 2) A paging request. At time of writing, we have build a proper abstraction for representing
+  * these, but for now they just return a Source[ResponseType, NotUser]. But this is subject to
+  * change, as we should find a general abstraction for these type of calls, that both handles
+  * errors properly, and also gives the client the possibility to actually use paging if needed.
+  */
 trait TwilioClient {
 
+  /** Return sub client for the Twilio IAM API */
   def iam: TwilioClientIam
 
+  /** Return sub client for the Twilio voice API */
   def voice: TwilioClientVoice
 
+  /** Return sub client for the Twilio messaging API
+    */
   def messaging: TwilioClientMessaging
 
+  /** Return sub client for the Twilio phoneNumber API */
   def phoneNumber: TwilioClientPhoneNumber
 }
 
---
+++ src/main/scala/com/dixa/twilio/client/package.scala
@@ -0,0 +1,7 @@
+package com.dixa.twilio
+
+/** Main package for the Twilio client code.
+  *
+  * All public classes, that are not directly related to one of twilio sub APIs is placed here.
+  */
+package object client {}
