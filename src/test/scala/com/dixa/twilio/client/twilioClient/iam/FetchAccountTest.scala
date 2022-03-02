0fb2ecd4da5cef7faf81aef4ace869f444bfe4dd
Anders Kreinøe <ankr@dixa.com>
2022-03-02 08:07:43 +0100

    added for now empty test file for fetching an account.

---
+++ src/test/scala/com/dixa/twilio/client/twilioClient/iam/FetchAccountTest.scala
@@ -0,0 +1,17 @@
+package com.dixa.twilio.client.twilioClient.iam
+
+import com.dixa.twilio.client.iam.TwilioClientIam
+import com.dixa.twilio.client.twilioClient.TwilioClientTest
+
+final class FetchAccountTest extends TwilioClientTest {
+
+  classOf[TwilioClientIam].getSimpleName when {
+    "Asked to fetch an account" should {
+
+      "Return the account that it receives from twilio" in {
+        fail("Test not implemented yet.")
+      }
+    }
+  }
+
+}
