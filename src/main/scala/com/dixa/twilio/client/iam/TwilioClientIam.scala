70b40ea1a351a9ba81887a02ac12d60735fde1dc
Anders Kreinøe <ankr@dixa.com>
2022-03-04 09:02:50 +0100

    Added scaladoc to TwilioClientIam.accountFetch

--- src/main/scala/com/dixa/twilio/client/iam/TwilioClientIam.scala
+++ src/main/scala/com/dixa/twilio/client/iam/TwilioClientIam.scala
@@ -7,6 +7,8 @@ import com.dixa.twilio.client.model.iam.TwilioAccount
 
 trait TwilioClientIam {
 
+  /** Fetch a single account for an account sid.
+    */
   def accountFetch: AccountFetchRequestExecutor
 
   /** Fetch all Twilio accounts
