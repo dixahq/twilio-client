0782112670228d36b5ccb9108d6e561cd8825e21
Anders Kreinøe <ankr@dixa.com>
2022-05-06 09:10:06 +0200

    Added the possibility to construct a Twiml Response type from a String.

--- src/main/scala/com/dixa/twilio/model/twiml/Response.scala
+++ src/main/scala/com/dixa/twilio/model/twiml/Response.scala
@@ -1,38 +1,64 @@
 package com.dixa.twilio.model.twiml
 
-sealed abstract class Response(val verbs: Seq[TwimlElement.Verb]) extends TwimlElement {
-
-  def canEqual(other: Any): Boolean = other.isInstanceOf[Response]
-
-  override def equals(other: Any): Boolean = other match {
-    case that: Response =>
-      (that canEqual this) &&
-      verbs == that.verbs
-    case _ => false
-  }
-
-  override def hashCode(): Int = {
-    val state = Seq(verbs)
-    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
-  }
-
-  override def toString = s"Response.${getClass.getSimpleName}($verbs)"
-
-  // format: off
-  override def xmlCompact: String =
-    s"""<?xml version="1.0" encoding="UTF-8"?><Response>${verbs.map(_.xmlCompact).mkString("")}</Response>"""
-  // format: on
-
-  override def xmlPretty: String =
-    s"""<?xml version="1.0" encoding="UTF-8"?>
-       |<Response>
-       |${verbs.map(v => s"  ${v.xmlPretty}").mkString(System.lineSeparator())}
-       |</Response>""".stripMargin
-}
+sealed trait Response extends TwimlElement
 
 object Response {
 
-  final class Verified private[Response] (v: Seq[TwimlElement.Verb]) extends Response(v)
+  sealed abstract class FromModel(val verbs: Seq[TwimlElement.Verb]) extends TwimlElement {
+
+    def canEqual(other: Any): Boolean = other.isInstanceOf[FromModel]
+
+    override def equals(other: Any): Boolean = other match {
+      case that: FromModel =>
+        (that canEqual this) &&
+        verbs == that.verbs
+      case _ => false
+    }
+
+    override def hashCode(): Int = {
+      val state = Seq(verbs)
+      state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+    }
+
+    override def toString = s"Response.${getClass.getSimpleName}($verbs)"
+
+    // format: off
+    override def xmlCompact: String =
+      s"""<?xml version="1.0" encoding="UTF-8"?><Response>${
+        verbs.map(_.xmlCompact).mkString("")
+      }</Response>"""
+    // format: on
+
+    override def xmlPretty: String =
+      s"""<?xml version="1.0" encoding="UTF-8"?>
+         |<Response>
+         |${verbs.map(v => s"  ${v.xmlPretty}").mkString(System.lineSeparator())}
+         |</Response>""".stripMargin
+  }
+
+  final class Verified private[Response] (v: Seq[TwimlElement.Verb]) extends FromModel(v)
+  sealed trait Unverified                                            extends Response
+
+  final class UnverifiedFromString private[Response] (val suppliedTwiml: String)
+      extends Unverified() {
+
+    override def toString = s"Response.${getClass.getSimpleName}($suppliedTwiml)"
+
+    override def xmlCompact: String = suppliedTwiml
+
+    override def xmlPretty: String = suppliedTwiml
+
+    override def equals(other: Any): Boolean = other match {
+      case that: UnverifiedFromString =>
+        suppliedTwiml == that.suppliedTwiml
+      case _ => false
+    }
+
+    override def hashCode(): Int = {
+      val state = Seq(suppliedTwiml)
+      state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+    }
+  }
 
   final class Builder[B <: PhantomTypes.Buildable, V <: PhantomTypes.Verified] private[Response] (
       verbs: Vector[TwimlElement.Verb]
@@ -48,10 +74,14 @@ object Response {
   }
 
   type BuilderStartState = Builder[PhantomTypes.BuildableFalse, PhantomTypes.VerifiedTrue]
-  type BuildFunction[A <: Response] = BuilderStartState => A
+  type BuildFunction[A <: FromModel] = BuilderStartState => A
 
-  def build[A <: Response](fun: BuildFunction[A]): A = fun(
+  def build[A <: FromModel](fun: BuildFunction[A]): A = fun(
     new BuilderStartState(Vector.empty)
+  )
+
+  def fromString(suppliedTwiml: String): UnverifiedFromString = new UnverifiedFromString(
+    suppliedTwiml
   )
 
 }
--- src/test/scala/com/dixa/twilio/model/twiml/ResponseTest.scala
+++ src/test/scala/com/dixa/twilio/model/twiml/ResponseTest.scala
@@ -6,6 +6,21 @@ final class ResponseTest extends AnyWordSpec {
 
   s"${classOf[Response].getSimpleName}" when {
 
+    "constructed from a pure string" should {
+
+      "return UnverifiedFromString instance that use supplied xml as both pretty and compact print" in {
+        val expectedXmlPretty =
+          s"""<?xml version="1.0" encoding="UTF-8"?>
+             |<Response>
+             |  <Say>Hello World</Say>
+             |</Response>""".stripMargin
+        val result: Response.UnverifiedFromString = Response.fromString(expectedXmlPretty)
+        assert(result.isInstanceOf[Response.Unverified])
+        assert(result.xmlPretty === expectedXmlPretty)
+        assert(result.xmlCompact === expectedXmlPretty)
+      }
+    }
+
     "Constructing responses with Say directives" should {
 
       "Be able to construct a response with a simple Say directive in a typesafe manner" in {
