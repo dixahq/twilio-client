6de0835467bf03fa93ed66e83bf891113692e654
Anders Kreinøe <ankr@dixa.com>
2022-05-09 22:36:20 +0200

    Refactored the Response tests a bit, so it's not one big file.

---
+++ src/test/scala/com/dixa/twilio/model/twiml/ResponseMiscTest.scala
@@ -0,0 +1,59 @@
+package com.dixa.twilio.model.twiml
+
+import org.scalatest.wordspec.AnyWordSpec
+
+final class ResponseMiscTest extends AnyWordSpec {
+  s"${classOf[Response].getSimpleName}" when {
+
+    "constructed from a pure string" should {
+
+      "return UnverifiedFromString instance that use supplied xml as both pretty and compact print" in {
+        val expectedXmlPretty =
+          s"""<?xml version="1.0" encoding="UTF-8"?>
+             |<Response>
+             |  <Say>Hello World</Say>
+             |</Response>""".stripMargin
+        val result: Response.UnverifiedFromString = Response.fromString(expectedXmlPretty)
+        assert(result.isInstanceOf[Response.Unverified])
+        assert(result.xmlPretty === expectedXmlPretty)
+        assert(result.xmlCompact === expectedXmlPretty)
+      }
+    }
+
+    "constructed from a builder but including a custome verb" should {
+
+      "not allow to call buildVerified" in {
+        assertTypeError(
+          """final class TestCustomVerb extends TwimlElement.Verb {
+            |          override def xmlCompact: String = "<CustomVerb>Hello<CustomVerb>"
+            |          override def xmlPretty: String  = xmlCompact
+            |}
+            |
+            |Response.build { responseBuilder =>
+            |          responseBuilder.addCustomVerb(new TestCustomVerb).buildVerified()
+            |}
+            |""".stripMargin
+        )
+      }
+
+      "return a instance that is both FromModel and Unverified" in {
+        final class TestCustomVerb extends TwimlElement.Verb {
+          override def xmlCompact: String = """<CustomVerb>Hello<CustomVerb>"""
+
+          override def xmlPretty: String = xmlCompact
+        }
+        val result: Response.UnverifiedFromModel = Response.build { responseBuilder =>
+          responseBuilder.addCustomVerb(new TestCustomVerb).buildUnverified()
+        }
+        assert(result.isInstanceOf[Response.FromModel])
+        assert(
+          result.xmlPretty ==
+            s"""<?xml version="1.0" encoding="UTF-8"?>
+               |<Response>
+               |  <CustomVerb>Hello<CustomVerb>
+               |</Response>""".stripMargin
+        )
+      }
+    }
+  }
+}
