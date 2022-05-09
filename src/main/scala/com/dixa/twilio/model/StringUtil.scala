120fb4565fe0b68c542ef8b239f201ba7287d115
Anders Kreinøe <ankr@dixa.com>
2022-05-10 00:55:43 +0200

    Made SayVerb support xml reserved chars in its text input.

--- src/main/scala/com/dixa/twilio/model/StringUtil.scala
+++ src/main/scala/com/dixa/twilio/model/StringUtil.scala
@@ -6,4 +6,17 @@ private[model] object StringUtil {
 
   private[model] def indentEveryLineWith2Spaces(input: String): String =
     input.lines.map(twoSpaecs + _).mkString(System.lineSeparator())
+
+  /** Escape all the XML special chars from this string.
+    *
+    * Source of list:
+    * https://en.wikipedia.org/wiki/List_of_XML_and_HTML_character_entity_references#Predefined%5Fentities%5Fin%5FXML
+    */
+  private[model] def xmlEscape(input: String): String = input
+    .replace("&", "&amp;") // Important that this goes first, as they other adds & chars.
+    .replace("\"", "&quot;")
+    .replace("'", "&apos;")
+    .replace("<", "&lt;")
+    .replace(">", "&gt;")
+
 }
