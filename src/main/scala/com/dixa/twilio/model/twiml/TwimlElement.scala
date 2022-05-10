894cbd4bd5d0db1f8591f57bc4f0ddf3d3d32000
Anders Kreinøe <ankr@dixa.com>
2022-05-10 16:10:07 +0200

    Added some documentation to TwimlElement.Root

--- src/main/scala/com/dixa/twilio/model/twiml/TwimlElement.scala
+++ src/main/scala/com/dixa/twilio/model/twiml/TwimlElement.scala
@@ -8,7 +8,13 @@ sealed trait TwimlElement {
 }
 
 object TwimlElement {
-  trait Verb                            extends TwimlElement
-  trait Noun                            extends TwimlElement
+  trait Verb extends TwimlElement
+  trait Noun extends TwimlElement
+
+  /** Abstraction over the root element of TwiML.
+    *
+    * Response is the only valid root element of TwiML, and therefore also the only existing and
+    * allowed implementation of Root.
+    */
   abstract class Root private[twiml] () extends TwimlElement
 }
