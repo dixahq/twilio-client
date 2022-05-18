548887a0464ff024299785cb325ad0ab830ec64e
Anders Kreinøe <ankr@dixa.com>
2022-05-18 16:36:46 +0200

    Added safe alternative to construct Call.Sid

--- src/main/scala/com/dixa/twilio/model/voice/Call.scala
+++ src/main/scala/com/dixa/twilio/model/voice/Call.scala
@@ -22,27 +22,38 @@ object Call {
     * The twilio documentation about it can be found here:
     * https://support.twilio.com/hc/en-us/articles/223180488-What-is-a-Call-SID-
     */
-  final case class Sid(override val toString: String) {
-    if (toString.isEmpty) throw Sid.ArgumentEmptyException()
-    if (!toString.startsWith("CA")) throw Sid.ArgumentMissingCaPrefixException(toString)
-    if (toString.length != 34) throw Sid.ArgumentLengthException(toString)
-  }
+  final case class Sid private (override val toString: String)
 
   object Sid {
-    private val conformToString = "Callsid is a 34 character string that starts with CA"
+
+    def apply(input: String): Sid = safe(input).toTry.get
+
+    def safe(input: String): Either[CreationException, Sid] = {
+      if (input.isEmpty) Left(Sid.ArgumentEmptyException())
+      else if (!input.startsWith("CA")) Left(Sid.ArgumentMissingCaPrefixException(input))
+      else if (input.length != 34) Left(Sid.ArgumentLengthException(input))
+      else Right(new Sid(input))
+    }
+
+    sealed trait CreationException extends RuntimeException
 
     final case class ArgumentEmptyException()
         extends IllegalArgumentException(s"Empty string does not conform to: $conformToString")
+        with CreationException
 
     final case class ArgumentMissingCaPrefixException(argument: String)
         extends IllegalArgumentException(
           s"$conformToString does not start with CA and therefor not conform to: $conformToString"
         )
+        with CreationException
 
     final case class ArgumentLengthException(argument: String)
         extends IllegalArgumentException(
           s"$argument has length not conforming to: $conformToString"
         )
+        with CreationException
+
+    private val conformToString = "Callsid is a 34 character string that starts with CA"
 
   }
 
--- src/test/scala/com/dixa/twilio/model/voice/CallSidTest.scala
+++ src/test/scala/com/dixa/twilio/model/voice/CallSidTest.scala
@@ -37,5 +37,46 @@ class CallSidTest extends AnyWordSpec {
         }
       }
     }
+
+    "constructed with safe method" should {
+
+      "return right on valid input" in {
+        val asString = "CAxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+        val resultE: Either[Call.Sid.CreationException, Call.Sid] = Call.Sid.safe(asString)
+        val result = resultE.getOrElse(fail(s"Expected right side here but was: $resultE"))
+        assert(result.toString === asString)
+      }
+
+      "return Left on empty strings as input" in {
+        val resultE  = Call.Sid.safe("")
+        val expected = Left(Call.Sid.ArgumentEmptyException())
+        assert(resultE === expected)
+      }
+
+      "not accept input that does not start with CA" in {
+        val asString = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        val resultE  = Call.Sid.safe(asString)
+        val expected = Left(Call.Sid.ArgumentMissingCaPrefixException(asString))
+        assert(resultE === expected)
+      }
+
+      "not accept input that is not 31 chars long (Should always be CA plus 32 chars" in {
+        val asString = "CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        val resultE  = Call.Sid.safe(asString)
+        val expected = Left(Call.Sid.ArgumentLengthException(asString))
+        assert(resultE === expected)
+      }
+
+      "not accept input that is not 33 chars long (Should always be CA plus 32 chars" in {
+        val asString = "CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        val resultE  = Call.Sid.safe(asString)
+        val expected = Left(Call.Sid.ArgumentLengthException(asString))
+        assert(resultE === expected)
+      }
+    }
+
+    "not allow using the constructor, to bypass validation" in {
+      assertDoesNotCompile("""new Call.Sid("NotValidInput")""")
+    }
   }
 }
