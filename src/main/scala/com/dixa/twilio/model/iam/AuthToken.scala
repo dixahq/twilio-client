91bea37a2efa76592f2e738d0882842d7bd80d85
Anders Kreinøe <ankr@dixa.com>
2022-09-05 20:45:44 +0200

    Added scaladoc to AuthToken

--- src/main/scala/com/dixa/twilio/model/iam/AuthToken.scala
+++ src/main/scala/com/dixa/twilio/model/iam/AuthToken.scala
@@ -1,10 +1,25 @@
 package com.dixa.twilio.model.iam
 
+/** Represent a AuthToken
+  *
+  * There is a total of 3 implementations of AuthToken:
+  *
+  *   - [[AuthToken.Primary]]
+  *   - [[AuthToken.Secondary]]
+  *   - [[AuthToken.UnknownType]]
+  *
+  * The first two represent the two known types of AuthTokens that exists in Twilio. Because of
+  * that, they are also subtypes of [[AuthToken.KnownType]].
+  *
+  * [[AuthToken.UnknownType]] can be used in cases where you don't know, or don't care what kind of
+  * auth token you are dealing with.
+  */
 sealed trait AuthToken {
 
+  /** String representation of the auth token */
   def asString: String
 
-  /** Will always return AuthToken.{ImplementationName}(***) to not accidentially log auth tokens */
+  /** Will always return AuthToken.{ImplementationName}(***) to not accidentally log auth tokens */
   override final def toString: String = s"AuthToken.${getClass.getSimpleName}(***)"
 }
 
