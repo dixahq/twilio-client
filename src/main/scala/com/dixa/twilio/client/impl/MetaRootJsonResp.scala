0db8b0261884c176f079359f70a644e1496e2f3f
Anders Kreinøe <ankr@dixa.com>
2022-09-13 12:32:47 +0200

    Added a link to twilio documentation in MetaRootJsonResp

--- src/main/scala/com/dixa/twilio/client/impl/MetaRootJsonResp.scala
+++ src/main/scala/com/dixa/twilio/client/impl/MetaRootJsonResp.scala
@@ -1,6 +1,6 @@
 package com.dixa.twilio.client.impl
 
-//  Full meta json object likes like this, but for now we only need the nex_page_url:
+//  Full meta json object looks like this, but for now we only need the nex_page_url:
 //  "meta": {
 //    "page": 1,
 //    "page_size": 2,
@@ -10,6 +10,9 @@ package com.dixa.twilio.client.impl
 //    "next_page_url": null,
 //    "key": "services"
 //  }
+//
+// There is no direct Twilio documentation on this, as it is documented under each resource using it.
+// But an example of such a resource is the Service in the messaging API: https://www.twilio.com/docs/messaging/services/api#
 
 private[client] final case class MetaRootJsonResp(meta: MetaRootJsonResp.MetaJsonRep)
 
