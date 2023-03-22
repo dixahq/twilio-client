0cac243202879f27fa4f38af405cd0aced82928f
Mads Fox <msf@dixa.com>
2023-03-22 14:14:00 +0100

    scalafmt

--- src/main/scala/com/dixa/twilio/model/voice/Conference.scala
+++ src/main/scala/com/dixa/twilio/model/voice/Conference.scala
@@ -153,12 +153,12 @@ object Conference {
 
     object Status extends EnumWithTwilioString[Status] {
       override val values: immutable.IndexedSeq[Status] = findValues
-      case object Queued extends Status("queued", isActive = true)
+      case object Queued     extends Status("queued", isActive = true)
       case object Connecting extends Status("connecting", isActive = true)
-      case object Ringing extends Status("ringing", isActive = true)
-      case object Connected extends Status("connected", isActive = true)
-      case object Complete extends Status("complete", isActive = false)
-      case object Failed extends Status("failed", isActive = false)
+      case object Ringing    extends Status("ringing", isActive = true)
+      case object Connected  extends Status("connected", isActive = true)
+      case object Complete   extends Status("complete", isActive = false)
+      case object Failed     extends Status("failed", isActive = false)
     }
   }
 
