f8a3948b127aa55c53b29e2c69e0fb010612e327
MadsSFox <100705951+MadsSFox@users.noreply.github.com>
2023-04-26 09:44:03 +0200

    Cha 1080/add language support (#140)
    * CHA-1014 Refactored how XML is created in the TwiML model, so that it is both much simpler for subclasses, but also impossible to forget to escape reserved XML chars.
    
    * CHA-1014 Fixed scaladoc link error.
    
    * Added unchecked language support for twiml <Say> verb
    
    * Changed enum name
    
    ---------
    
    Co-authored-by: Anders Kreinøe <ankr@dixa.com>

--- src/test/scala/com/dixa/twilio/model/twiml/ResponseSayTest.scala
+++ src/test/scala/com/dixa/twilio/model/twiml/ResponseSayTest.scala
@@ -1,5 +1,6 @@
 package com.dixa.twilio.model.twiml
 
+import com.dixa.twilio.model.twiml.verb.SayVerb.{LanguageCode, Voice}
 import org.scalatest.wordspec.AnyWordSpec
 
 final class ResponseSayTest extends AnyWordSpec {
@@ -13,24 +14,32 @@ final class ResponseSayTest extends AnyWordSpec {
           val textToSay = """Twilio can you pronounce these reserved XML chars: "'<>&"""
           val textToSayEscaped =
             "Twilio can you pronounce these reserved XML chars: &quot;&apos;&lt;&gt;&amp;"
+
+          val language = LanguageCode.`en-GB`
+          val voice    = Voice.`alice`
           val result: Response.Verified = Response.build { responseBuilder =>
             responseBuilder
               .addSay { sayBuilder =>
-                sayBuilder.withText(textToSay).build
+                sayBuilder
+                  .withText(textToSay)
+                  .withLanguage(language)
+                  .withVoice(voice)
+                  .withLoop(5)
+                  .build
               }
               .buildVerified()
           }
 
           val xmlCompact = result.xmlCompact
           val expectedXmlCompact =
-            s"""<?xml version="1.0" encoding="UTF-8"?><Response><Say>$textToSayEscaped</Say></Response>"""
+            s"""<?xml version="1.0" encoding="UTF-8"?><Response><Say language="en-GB" voice="alice" loop="5">$textToSayEscaped</Say></Response>"""
           assert(xmlCompact == expectedXmlCompact)
 
           val xmlPretty = result.xmlPretty
           val expectedXmlPretty =
             s"""<?xml version="1.0" encoding="UTF-8"?>
                |<Response>
-               |  <Say>$textToSayEscaped</Say>
+               |  <Say language="en-GB" voice="alice" loop="5">$textToSayEscaped</Say>
                |</Response>""".stripMargin
           assert(xmlPretty === expectedXmlPretty)
         }
