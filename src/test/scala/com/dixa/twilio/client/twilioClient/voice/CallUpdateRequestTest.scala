57eb74fb9733dd9cbfad39c136c5421fed76fda3
Ugnė Valionytė <uva@dixa.com>
2023-06-13 15:41:22 +0200

    update the unrelated call update test

--- src/test/scala/com/dixa/twilio/client/twilioClient/voice/CallUpdateRequestTest.scala
+++ src/test/scala/com/dixa/twilio/client/twilioClient/voice/CallUpdateRequestTest.scala
@@ -1,119 +1,141 @@
 package com.dixa.twilio.client.twilioClient.voice
 
-import com.dixa.twilio.model.HttpMethod
+import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
 import org.scalatest.wordspec.AnyWordSpec
 
 // The test results are cached so may need to run sbt clean when test code is changed
 
 final class CallUpdateRequestTest extends AnyWordSpec {
 
-  "requires accountSid" in {
-    assertTypeError(
-      """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
-        |    import com.dixa.twilio.model.twiml.Response
-        |    import com.dixa.twilio.model.voice.Call
-        |    CallUpdateRequestExecutor.CallUpdateRequest.build(
-        |      _.withTwiml(Response.build(_.addHangup(_.build()).buildVerified()))
-        |        .withCallSid(Call.Sid.unsafe("some_call_sid"))
-        |        .build()
-        |    )""".stripMargin
-    )
-  }
+  classOf[CallUpdateRequestExecutor.CallUpdateRequest].getSimpleName when {
 
-  "requires callSid" in {
-    assertTypeError(
-      """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
-        |  import com.dixa.twilio.model.callback.CallbackUrl
-        |  import com.dixa.twilio.model.iam.TwilioAccount
-        |  CallUpdateRequestExecutor.CallUpdateRequest.build(
-        |    _.withAccountSid(TwilioAccount.Sid.unsafe("some_account_sid"))
-        |      .withUrl(CallbackUrl("some_url"))
-        |      .build()
-        |  )""".stripMargin
-    )
-  }
+    "accountSid is not set" should {
+      "require accountSid" in {
+        assertTypeError(
+          """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
+            |    import com.dixa.twilio.model.twiml.Response
+            |    import com.dixa.twilio.model.voice.Call
+            |
+            |    CallUpdateRequestExecutor.CallUpdateRequest.build(
+            |      _.withTwiml(Response.build(_.addHangup(_.build()).buildVerified()))
+            |        .withCallSid(Call.Sid.unsafe("some_call_sid"))
+            |        .build()
+            |    )""".stripMargin
+        )
+      }
+    }
 
-  "does not allow to set url when twiml is already set" in {
-    assertTypeError(
-      """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
-        |      import com.dixa.twilio.model.callback.CallbackUrl
-        |      import com.dixa.twilio.model.iam.TwilioAccount
-        |      import com.dixa.twilio.model.twiml.Response
-        |      import com.dixa.twilio.model.voice.Call
-        |      CallUpdateRequestExecutor.CallUpdateRequest.build(
-        |        _.withAccountSid(TwilioAccount.Sid.unsafe("some_account_sid"))
-        |          .withTwiml(Response.build(_.addHangup(_.build()).buildVerified()))
-        |          .withCallSid(Call.Sid.unsafe("some_call_sid"))
-        |          .withUrl(CallbackUrl("some_url"))
-        |          .build()
-        |      )""".stripMargin
-    )
-  }
+    "callSid is not set" should {
+      "require callSid" in {
+        assertTypeError(
+          """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
+            |  import com.dixa.twilio.model.callback.CallbackUrl
+            |  import com.dixa.twilio.model.iam.TwilioAccount
+            |
+            |  CallUpdateRequestExecutor.CallUpdateRequest.build(
+            |    _.withAccountSid(TwilioAccount.Sid.unsafe("some_account_sid"))
+            |      .withUrl(CallbackUrl("some_url"))
+            |      .build()
+            |  )""".stripMargin
+        )
+      }
+    }
 
-  "requires url when method method is set" in {
-    assertTypeError(
-      """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
-        |  import com.dixa.twilio.model.iam.TwilioAccount
-        |  import com.dixa.twilio.model.twiml.Response
-        |  import com.dixa.twilio.model.voice.Call
-        |
-        |  CallUpdateRequestExecutor.CallUpdateRequest.build(
-        |    _.withAccountSid(TwilioAccount.Sid.unsafe("some_account_sid"))
-        |      .withCallSid(Call.Sid.unsafe("some_call_sid"))
-        |      .withTwiml(Response.build(_.addHangup(_.build()).buildVerified()))
-        |      .withMethod(HttpMethod.Post)
-        |      .build()
-        |  )""".stripMargin
-    )
-  }
+    "request is created with required accountSid and callSid attributes" should {
+      "require at least one update attribute" in {
+        assertTypeError(
+          """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
+            |  import com.dixa.twilio.model.iam.TwilioAccount
+            |  import com.dixa.twilio.model.twiml.Response
+            |  import com.dixa.twilio.model.voice.Call
+            |
+            |  CallUpdateRequestExecutor.CallUpdateRequest.build(
+            |    _.withAccountSid(TwilioAccount.Sid.unsafe("some_account_sid"))
+            |      .withCallSid(Call.Sid.unsafe("some_call_sid"))
+            |      .build()
+            |  )""".stripMargin
+        )
+      }
+    }
 
-  "requires fallbackUrl when fallbackMethod is set" in {
-    assertTypeError(
-      """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
-        |  import com.dixa.twilio.model.iam.TwilioAccount
-        |  import com.dixa.twilio.model.twiml.Response
-        |  import com.dixa.twilio.model.voice.Call
-        |
-        |  CallUpdateRequestExecutor.CallUpdateRequest.build(
-        |    _.withAccountSid(TwilioAccount.Sid.unsafe("some_account_sid"))
-        |      .withCallSid(Call.Sid.unsafe("some_call_sid"))
-        |      .withTwiml(Response.build(_.addHangup(_.build()).buildVerified()))
-        |      .withFallbackMethod(HttpMethod.Post)
-        |      .build()
-        |  )""".stripMargin
-    )
-  }
+    "both twiml and url are set" should {
+      "not allow to have both because only one is allowed" in {
+        assertTypeError(
+          """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
+            |      import com.dixa.twilio.model.callback.CallbackUrl
+            |      import com.dixa.twilio.model.iam.TwilioAccount
+            |      import com.dixa.twilio.model.twiml.Response
+            |      import com.dixa.twilio.model.voice.Call
+            |
+            |      CallUpdateRequestExecutor.CallUpdateRequest.build(
+            |        _.withAccountSid(TwilioAccount.Sid.unsafe("some_account_sid"))
+            |          .withTwiml(Response.build(_.addHangup(_.build()).buildVerified()))
+            |          .withCallSid(Call.Sid.unsafe("some_call_sid"))
+            |          .withUrl(CallbackUrl("some_url"))
+            |          .build()
+            |      )""".stripMargin
+        )
+      }
+    }
 
-  "requires statusCallbackUrl when statusCallbackMethod is set" in {
-    assertTypeError(
-      """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
-        |  import com.dixa.twilio.model.iam.TwilioAccount
-        |  import com.dixa.twilio.model.twiml.Response
-        |  import com.dixa.twilio.model.voice.Call
-        |
-        |  CallUpdateRequestExecutor.CallUpdateRequest.build(
-        |    _.withAccountSid(TwilioAccount.Sid.unsafe("some_account_sid"))
-        |      .withCallSid(Call.Sid.unsafe("some_call_sid"))
-        |      .withTwiml(Response.build(_.addHangup(_.build()).buildVerified()))
-        |      .withStatusCallBackMethod(HttpMethod.Post)
-        |      .build()
-        |  )""".stripMargin
-    )
-  }
+    "method is set" should {
+      "require url to be set" in {
+        assertTypeError(
+          """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
+            |  import com.dixa.twilio.model.iam.TwilioAccount
+            |  import com.dixa.twilio.model.twiml.Response
+            |  import com.dixa.twilio.model.voice.Call
+            |  import com.dixa.twilio.model.HttpMethod
+            |
+            |  CallUpdateRequestExecutor.CallUpdateRequest.build(
+            |    _.withAccountSid(TwilioAccount.Sid.unsafe("some_account_sid"))
+            |      .withCallSid(Call.Sid.unsafe("some_call_sid"))
+            |      .withTwiml(Response.build(_.addHangup(_.build()).buildVerified()))
+            |      .withMethod(HttpMethod.Post)
+            |      .build()
+            |  )""".stripMargin
+        )
+      }
+    }
 
-  "requires at least one update" in {
-    assertTypeError(
-      """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
-        |  import com.dixa.twilio.model.iam.TwilioAccount
-        |  import com.dixa.twilio.model.twiml.Response
-        |  import com.dixa.twilio.model.voice.Call
-        |
-        |  CallUpdateRequestExecutor.CallUpdateRequest.build(
-        |    _.withAccountSid(TwilioAccount.Sid.unsafe("some_account_sid"))
-        |      .withCallSid(Call.Sid.unsafe("some_call_sid"))
-        |      .build()
-        |  )""".stripMargin
-    )
-  }
+    "fallbackMethod is set" should {
+      "require fallbackUrl to be set" in {
+        assertTypeError(
+          """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
+            |  import com.dixa.twilio.model.iam.TwilioAccount
+            |  import com.dixa.twilio.model.twiml.Response
+            |  import com.dixa.twilio.model.voice.Call
+            |  import com.dixa.twilio.model.HttpMethod
+            |
+            |  CallUpdateRequestExecutor.CallUpdateRequest.build(
+            |    _.withAccountSid(TwilioAccount.Sid.unsafe("some_account_sid"))
+            |      .withCallSid(Call.Sid.unsafe("some_call_sid"))
+            |      .withTwiml(Response.build(_.addHangup(_.build()).buildVerified()))
+            |      .withFallbackMethod(HttpMethod.Post)
+            |      .build()
+            |  )""".stripMargin
+        )
+      }
+    }
 
+    "statusCallbackMethod is set" should {
+      "require statusCallbackUrl to be set" in {
+        assertTypeError(
+          """import com.dixa.twilio.client.voice.CallUpdateRequestExecutor
+            |  import com.dixa.twilio.model.iam.TwilioAccount
+            |  import com.dixa.twilio.model.twiml.Response
+            |  import com.dixa.twilio.model.voice.Call
+            |  import com.dixa.twilio.model.HttpMethod
+            |
+            |  CallUpdateRequestExecutor.CallUpdateRequest.build(
+            |    _.withAccountSid(TwilioAccount.Sid.unsafe("some_account_sid"))
+            |      .withCallSid(Call.Sid.unsafe("some_call_sid"))
+            |      .withTwiml(Response.build(_.addHangup(_.build()).buildVerified()))
+            |      .withStatusCallBackMethod(HttpMethod.Post)
+            |      .build()
+            |  )""".stripMargin
+        )
+      }
+    }
+  }
 }
