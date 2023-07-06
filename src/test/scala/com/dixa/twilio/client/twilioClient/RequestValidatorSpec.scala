29b4688c821f199f75a26899864025b533dc7710
Anders Kreinøe <ankr@dixa.com>
2023-07-06 13:31:36 +0200

    CHA-1160 Fixed the tests of RequestValidatorSpec, that was a little messed up and confused.

--- src/test/scala/com/dixa/twilio/client/twilioClient/RequestValidatorSpec.scala
+++ src/test/scala/com/dixa/twilio/client/twilioClient/RequestValidatorSpec.scala
@@ -57,8 +57,10 @@ class RequestValidatorSpec extends AnyFlatSpec with Matchers with ScalaFutures {
     // the http server has already performed once.
 
     val authToken = AuthToken.UnknownType("fakeToken")
-    // This signature is the one being expected for the provided url as is. even though it has none encoded : chars in the query params.
-    val xTwilioSignature = XTwilioSignature("CMJUMMXVTgBR/1Nf17u/a0jL/wM=")
+    // This signature is the one being expected for the provided url when the query parameter has been re encoded.
+    // So it basical the signature expected for this URL:
+    // "https://sms-twilio.euw1.stag.dixa.io/v1/e7a04fc4-bba8-48a8-a92e-013606a188a6/sms?param1=that%3Ahas%3Achars%3Athat%3Awas%3Aoriginally%3Aencoded&param2=NoSpecialCharsHere#fragmentPart
+    val xTwilioSignature = XTwilioSignature("vXRQRiCcfD1DaD0g6Vs3LzkfJSY=")
 
     // provide an url where the correct url encoded %3A in the query params, has been replaced with the actual : that they represent.
     // URI abstraction layers will often print them like that, to make it more human readable, and as such, it's easy
@@ -74,6 +76,15 @@ class RequestValidatorSpec extends AnyFlatSpec with Matchers with ScalaFutures {
     // twilio actually seem to be a bit confused about this, and sometimes they send a signature, that matched the url where the query params is decoded.
     // None the less, from a logical perspective, it anyway makes sense to get a match, if the hash matched the exact input.
 
+    val authToken = AuthToken.UnknownType("fakeToken")
+    // This signature is the one being expected for the provided url as is. even though it has none encoded : chars in the query params.
+    val xTwilioSignature = XTwilioSignature("CMJUMMXVTgBR/1Nf17u/a0jL/wM=")
+
+    val requestUrl =
+      "https://sms-twilio.euw1.stag.dixa.io/v1/e7a04fc4-bba8-48a8-a92e-013606a188a6/sms?param1=that:has:chars:that:was:originally:encoded&param2=NoSpecialCharsHere#fragmentPart"
+    val validationSignature =
+      requestValidator.validate(requestUrl, authToken, requestParams, xTwilioSignature)
+    validationSignature shouldBe ValidationStatus.Valid
   }
 
   it should "encrypt properly when the url query params are already encoded correctly" in {
