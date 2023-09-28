3d4b9d8b9b07071dba833aa22048b5afd9ae9a04
Ugnė Valionytė <uva@dixa.com>
2023-09-28 11:05:09 +0200

    Change from vector to a Set because it is faster :D

--- src/main/scala/com/dixa/twilio/model/messaging/MessageBody.scala
+++ src/main/scala/com/dixa/twilio/model/messaging/MessageBody.scala
@@ -4,7 +4,7 @@ final case class MessageBody(override val toString: String)
 
 object MessageBody {
 
-  private val gsm7Chars = Vector('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+  private val gsm7Chars = Set('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
     'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '#', ' ', '"', '%', '&', '\'', '(',
