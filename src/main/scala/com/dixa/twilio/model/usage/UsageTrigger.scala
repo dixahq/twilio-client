2489db9a3ad94c4ac26940013602baf8ef5c2499
Mads Fox <msf@dixa.com>
2023-10-05 12:57:15 +0200

    Adding model UsageTrigger

---
+++ src/main/scala/com/dixa/twilio/model/usage/UsageTrigger.scala
@@ -0,0 +1,75 @@
+package com.dixa.twilio.model.usage
+
+import com.dixa.twilio.model.SidAbstract.Prefix
+import com.dixa.twilio.model.callback.CallbackUrl
+import com.dixa.twilio.model.iam.TwilioAccount
+import com.dixa.twilio.model.{EnumWithTwilioString, HttpMethod, SidAbstract, TwilioStringValue}
+
+import java.time.Instant
+import scala.collection.immutable
+
+/** Represent a Account or a Subaccount at Twilio
+  */
+final case class UsageTrigger(
+    accountSid: TwilioAccount.Sid,
+    callBackMethod: HttpMethod,
+    callbackUrl: CallbackUrl,
+    currentValue: UsageTrigger.CurrentValue,
+    dateCreated: Instant,
+    dateFired: Option[Instant],
+    dateUpdated: Instant,
+    friendlyName: UsageTrigger.FriendlyName,
+    recurring: Option[UsageTrigger.Recurring],
+    sid: UsageTrigger.Sid,
+    triggerBy: UsageTrigger.TriggerBy,
+    triggerValue: UsageTrigger.TriggerValue,
+    usageCategory: UsageTrigger.UsageCategory
+)
+
+object UsageTrigger {
+
+  final case class CurrentValue(override val toString: String) extends TwilioStringValue
+  final case class TriggerValue(override val toString: String) extends TwilioStringValue
+  final case class FriendlyName(override val toString: String) extends TwilioStringValue
+  final case class Sid private (override val toString: String) extends SidAbstract
+
+  object Sid extends SidAbstract.SidCompanionObject[Sid](List(Prefix("UT")), new Sid(_))
+
+  sealed abstract class Recurring(override val twilioString: String)
+      extends EnumWithTwilioString.EnumEntry
+  object Recurring extends EnumWithTwilioString[Recurring] {
+    override val values: immutable.IndexedSeq[Recurring] = findValues
+
+    case object Daily   extends Recurring("daily")
+    case object Monthly extends Recurring("monthly")
+    case object Yearly  extends Recurring("yearly")
+  }
+
+  sealed abstract class TriggerBy(override val twilioString: String)
+      extends EnumWithTwilioString.EnumEntry
+  object TriggerBy extends EnumWithTwilioString[TriggerBy] {
+    override val values: immutable.IndexedSeq[TriggerBy] = findValues
+
+    case object Count extends TriggerBy("count")
+    case object Usage extends TriggerBy("usage")
+    case object Price extends TriggerBy("price")
+  }
+
+  sealed abstract class UsageCategory(override val twilioString: String)
+      extends EnumWithTwilioString.EnumEntry
+
+  object UsageCategory extends EnumWithTwilioString[UsageCategory] {
+    override val values: immutable.IndexedSeq[UsageCategory] = findValues
+
+    case object Calls          extends UsageCategory("calls")
+    case object Sms            extends UsageCategory("sms")
+    case object PfaxMinutes    extends UsageCategory("pfax-minutes")
+    case object PfaxPages      extends UsageCategory("pfax-pages")
+    case object Phonenumbers   extends UsageCategory("phonenumbers")
+    case object Secordings     extends UsageCategory("recordings")
+    case object Transcriptions extends UsageCategory("transcriptions")
+    case object Pv             extends UsageCategory("pv")
+    case object Totalprice     extends UsageCategory("totalprice")
+
+  }
+}
