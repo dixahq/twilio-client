53d9ef6af4d3b0004edd95af979c82ba5c034a38
Anders Kreinøe <ankr@dixa.com>
2023-10-13 14:28:23 +0200

    CHA-1160 Implemented support for deleting an Application.

---
+++ src/main/scala/com/dixa/twilio/client/general/ApplicationDeleteRequestExecutor.scala
@@ -0,0 +1,125 @@
+package com.dixa.twilio.client.general
+
+import com.dixa.twilio.client.RequestExecutor.ApiExceptionWrapper
+import com.dixa.twilio.client.{ApiException, SingleRequestExecutor}
+import com.dixa.twilio.model.Funit
+import com.dixa.twilio.model.general.Application
+import com.dixa.twilio.model.iam.TwilioAccount
+
+trait ApplicationDeleteRequestExecutor
+    extends SingleRequestExecutor[
+      ApplicationDeleteRequestExecutor.ApplicationDeleteRequest,
+      ApplicationDeleteRequestExecutor.ApplicationDeleteException,
+      Funit
+    ] {
+
+  override protected type ApiExceptionWrapper =
+    ApplicationDeleteRequestExecutor.ApplicationDeleteException.Api
+
+  override protected type UnspecifiedException =
+    ApplicationDeleteRequestExecutor.ApplicationDeleteException.Unspecified
+}
+
+object ApplicationDeleteRequestExecutor {
+
+  sealed trait ApplicationDeleteRequest {
+    def accountSid: TwilioAccount.Sid
+    def sid: Application.Sid
+  }
+
+  object ApplicationDeleteRequest {
+
+    type BuilderStartState = Builder[PhantomTypes.AccountSidSetFalse, PhantomTypes.SidSetFalse]
+
+    /** Phantom types used to enforce compile time constraints on the Builder */
+    object PhantomTypes {
+
+      sealed trait AccountSidSet
+      sealed trait AccountSidSetTrue  extends AccountSidSet
+      sealed trait AccountSidSetFalse extends AccountSidSet
+
+      sealed trait SidSet
+      sealed trait SidSetTrue  extends SidSet
+      sealed trait SidSetFalse extends SidSet
+    }
+
+    final class Builder[
+        AccountSidSet <: PhantomTypes.AccountSidSet,
+        SidSet <: PhantomTypes.SidSet
+    ] private[ApplicationDeleteRequest] (
+        accountSid: Option[TwilioAccount.Sid],
+        sid: Option[Application.Sid]
+    ) {
+
+      private def copy[
+          NewAccountSidSet <: PhantomTypes.AccountSidSet,
+          NewSidSet <: PhantomTypes.SidSet
+      ](
+          accountSid: Option[TwilioAccount.Sid] = accountSid,
+          sid: Option[Application.Sid] = sid
+      ): Builder[
+        NewAccountSidSet,
+        NewSidSet
+      ] =
+        new Builder(
+          accountSid,
+          sid
+        )
+
+      /** The SID of the Account that will delete the resource. */
+      def withAccountSid(
+          accountSid: TwilioAccount.Sid
+      ): Builder[
+        PhantomTypes.AccountSidSetTrue,
+        SidSet
+      ] =
+        copy(accountSid = Some(accountSid))
+
+      /** The SID of the application to delete */
+      def withSid(sid: Application.Sid): Builder[AccountSidSet, PhantomTypes.SidSetTrue] =
+        copy(sid = Some(sid))
+
+      def build()(
+          implicit accountSidSetEv: AccountSidSet =:= PhantomTypes.AccountSidSetTrue,
+          sidSetEv: SidSet =:= PhantomTypes.SidSetTrue
+      ): ApplicationDeleteRequest = RequestImpl(accountSid.get, sid.get)
+    }
+
+    object Builder {
+      val empty = new BuilderStartState(None, None)
+    }
+
+    def build(fun: BuilderStartState => ApplicationDeleteRequest): ApplicationDeleteRequest =
+      fun(Builder.empty)
+  }
+
+  private final case class RequestImpl(
+      accountSid: TwilioAccount.Sid,
+      sid: Application.Sid
+  ) extends ApplicationDeleteRequest
+
+  sealed trait ApplicationDeleteException extends RuntimeException
+
+  object ApplicationDeleteException {
+    final case class Api(cause: ApiException)
+        extends RuntimeException(cause)
+        with ApplicationDeleteException
+        with ApiExceptionWrapper
+
+    final case class Unspecified(msg: Option[String], cause: Option[Throwable])
+        extends RuntimeException(
+          msg.getOrElse(
+            "Unspecified error happened trying to delete application"
+          ),
+          cause.orNull
+        )
+        with ApplicationDeleteException
+
+    object Unspecified {
+      def apply(msg: String) = new Unspecified(Some(msg), None)
+
+      def apply(cause: Throwable) = new Unspecified(Option(cause.getMessage), Some(cause))
+    }
+  }
+
+}
---
+++ src/test/scala/com/dixa/twilio/client/twilioClient/general/ApplicationDeleteTest.scala
@@ -0,0 +1,59 @@
+package com.dixa.twilio.client.twilioClient.general
+
+import com.dixa.twilio.CommonFixtures
+import com.dixa.twilio.client.general.ApplicationDeleteRequestExecutor
+import com.dixa.twilio.client.messaging.TwilioClientMessaging
+import com.dixa.twilio.client.twilioClient.TwilioClientTest
+import com.dixa.twilio.client.{TwilioClient, TwilioTestConstants}
+import com.dixa.twilio.model.Funit
+import com.dixa.twilio.model.general.Application
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+
+import scala.concurrent.Future
+
+final class ApplicationDeleteTest extends TwilioClientTest {
+  classOf[TwilioClientMessaging].getSimpleName when {
+
+    "ask to delete an Application" should {
+      "ask twilio to delete it" in {
+
+        val request = ApplicationDeleteRequestExecutor.ApplicationDeleteRequest.build(
+          _.withAccountSid(CommonFixtures.accountSid1)
+            .withSid(Application.Sid.unsafe("APXXXXXXXXXXXXXXXXXXXXXXXXXXXXX536"))
+            .build()
+        )
+
+        wireMockServer.stubFor(
+          WireMock
+            .delete(
+              WireMock.urlPathEqualTo(
+                s"/2010-04-01/Accounts/${CommonFixtures.accountSid1}/Applications/APXXXXXXXXXXXXXXXXXXXXXXXXXXXXX536.json"
+              )
+            )
+            .withBasicAuth("ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", "testPassword")
+            .willReturn(
+              aResponse()
+                .withStatus(204)
+            )
+        )
+
+        val connSettings = TwilioTestConstants.connSettings(wireMockServer.port())
+        val instance: ApplicationDeleteRequestExecutor =
+          TwilioClient.defaultImpl().general.applicationDelete
+        val resultFut: Future[
+          Either[ApplicationDeleteRequestExecutor.ApplicationDeleteException, Funit]
+        ] = {
+          instance.run(connSettings, request)
+        }
+        resultFut.map { result =>
+          val succResult = result.getOrElse {
+            val e = result.left.getOrElse(fail("No success or either, how can that happen :D"))
+            fail("expected successfully result here", e)
+          }
+          assert(succResult === Funit)
+        }
+      }
+    }
+  }
+}
