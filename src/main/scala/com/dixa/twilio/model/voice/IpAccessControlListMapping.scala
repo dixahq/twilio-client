a9a0b05f46d46465316354fc92e66a159f3ad567
Anders Kreinøe <ankr@dixa.com>
2023-10-23 14:56:43 +0200

    Update src/main/scala/com/dixa/twilio/model/voice/IpAccessControlListMapping.scala
    Co-authored-by: Ugnė Valionytė <91066435+UgneValio@users.noreply.github.com>

--- src/main/scala/com/dixa/twilio/model/voice/IpAccessControlListMapping.scala
+++ src/main/scala/com/dixa/twilio/model/voice/IpAccessControlListMapping.scala
@@ -4,15 +4,15 @@ import com.dixa.twilio.model.iam.TwilioAccount
 
 /** Represent a mapping between a [[SipDomain]] and a [[IpAccessControlList]].
   *
-  * At point of writing this, the Twilio documentation does not corospond exactly to how their API
-  * actually represent this. Because of this, this class diverde a bit from the Twilio doc on these
+  * At point of writing this, the Twilio documentation does not correspond exactly to how their API
+  * actually represent this. Because of this, this class diverge a bit from the Twilio doc on these
   * points:
-  *   - This resource does not have it's own sid. The sid parameter is always the value of the
+  *   - This resource does not have its own sid. The sid parameter is always the value of the
   *     SipDomains, and as such this class calls it SipDomainSid.
   *   - Twilio does not seem to record individual timestamp on the mappings, and instead the
   *     resource always returns the timestamps of the parent SipDomain resource. Because of that,
-  *     this class has not timestamps.
-  *   - There is no friendly name, as twilio does not record a friendly name specific for this sub
+  *     this class has no timestamps.
+  *   - There is no friendly name, as Twilio does not record a friendly name specific for this sub
   *     resource. The friendly name returned by the Twilio API is the friendly name of the linked
   *     [[IpAccessControlList]]
   *
