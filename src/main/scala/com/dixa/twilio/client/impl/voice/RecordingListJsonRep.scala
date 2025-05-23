6f278ac5a2a859afec80c7b287ad7735441421d4
MadsSFox <100705951+MadsSFox@users.noreply.github.com>
2025-05-23 15:11:04 +0200

    Fix/map json parsing error (#281)
    * Map json parsing exception for debugging
    
    * clean up
    
    * clean up
    
    * Add better logging
    
    * Add better logging
    
    * Make 'first_page_uri' optional and add optional 'total' to RecordingListJsonRep

--- src/main/scala/com/dixa/twilio/client/impl/voice/RecordingListJsonRep.scala
+++ src/main/scala/com/dixa/twilio/client/impl/voice/RecordingListJsonRep.scala
@@ -3,7 +3,7 @@ package com.dixa.twilio.client.impl.voice
 import com.dixa.twilio.client.impl.TwilioClientPickler.{macroR, Reader}
 
 private[voice] final case class RecordingListJsonRep(
-    first_page_uri: String,
+    first_page_uri: Option[String] = None,
     end: Int,
     recordings: List[RecordingJsonRep],
     previous_page_uri: Option[String] = None,
@@ -11,7 +11,8 @@ private[voice] final case class RecordingListJsonRep(
     page_size: Int,
     start: Int,
     next_page_uri: Option[String] = None,
-    page: Int
+    page: Int,
+    total: Option[Int] = None
 )
 
 private[voice] object RecordingListJsonRep {
